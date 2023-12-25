68f49acf7cf802811921067f58f9b082f2b8392c
Bohdan <gavriltsiv250@gmail.com>
2023-12-25 23:08:54 +0200

    -installed nodemon -added search of gif by api -added sending of gif to the bot to the bot

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+const tenorAxiosClient = axios.create({
+    baseURL: 'https://g.tenor.com/v2',
+});
 
 const tgToken = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(tgToken, { polling: true });
 
-bot.on('message', (msg) => {
-    console.log(msg);
+bot.on('message', async (msg) => {
 
-    const Hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.chat.id,"Hello dear user");
+    const params = {
+        key: process.env.TENOR_API_TOKEN,
+    };
+
+    if (msg.text.length !== 0) {
+        params.q = msg.text;
+        try {
+            const gifs = await tenorAxiosClient.get('/search', { params });
+            bot.sendAnimation(msg.chat.id, gifs.data.results[0].media_formats.gif.url);
+        } catch (err) {
+            console.log(err?.message);
+            bot.sendMessage(msg.chat.id, 'Something went wrong!');
+        }
     }
 });
